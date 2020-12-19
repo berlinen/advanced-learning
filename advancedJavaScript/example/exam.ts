8ae2392769bf61c02d319b8527ea9411bf658c0e
“berlinen” <1292393108@qq.com>
2020-12-19 13:49:24 +0800

    feat: update

--- advancedJavaScript/example/exam.ts
+++ advancedJavaScript/example/exam.ts
@@ -1,38 +1,10 @@
-const promise1: () => Promise<void> = () => Reflect.construct(Promise, [(resolve) => {
-  setTimeout(() => {
-    console.log('已完成 1')
-    resolve('promise 1')
-  }, 1000);
-}])
-
-const promise2: () => Promise<void> = () => Reflect.construct(Promise, [(resolve) => {
-  setTimeout(() => {
-    console.log('已完成 2')
-    resolve('promise 2')
-  }, 2000);
-}])
-
-// 使用generator函数
-
-function* gen() {
-  yield promise1();
-  yield promise2();
-}
-
-const g = gen();
-
-// g.next()
-// g.next()
-
-// s使用async await
-
-(async function () {
-  try {
-    const res = await promise1();
-    await promise2()
-    console.log('已完成')
-  } catch (e) {
-    console.log(e);
-    console.log('已拒绝')
-  }
-}())
+var promise1 = new Promise((resolve, reject) => {reject();});
+ promise1
+.then(null, function() { return 123;
+})
+.then(null, null) .then(null, null) .then(
+() => {
+console.log('promise2 已完成');
+},
+() => {
+console.log('promise2 已拒绝'); });
