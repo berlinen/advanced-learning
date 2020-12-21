86e9c4be1571b74c6233d30a02ac5343701df1b0
“berlinen” <1292393108@qq.com>
2020-12-21 11:29:14 +0800

    feat: protoType

--- advancedJavaScript/example/examaple.js
+++ advancedJavaScript/example/examaple.js
@@ -10,3 +10,27 @@ let p1 = new Player("white");
 console.log(Player.total);
 let p2 = new Player("black");
 console.log(Player.total); // 2
+
+function ObjectFactory () {
+  let obj = new Object();
+  let Constructor = [].shift.call(arguments);
+  obj.__proto__ = Constructor.protoType;
+  let ret = Constructor.apply(obj, arguments)
+  return typeof ret === 'object' ? obj : ret
+}
+
+function Parent () {}
+function Child () {
+  Parent.call(this, Array.from(arguments).slice(1))
+}
+
+// Child.prototype = new Parent();
+
+// let TemFunc = function() {};
+// TemFunc.prototype = Parent.prototype;
+// Child.prototype = new TemFunc()
+
+Object.create(Parent.prototype)
+
+
+Child.prototype.constructor = Child;
