7bbcdf992471177b4d48a2db51c353668e90728c
“berlinen” <1292393108@qq.com>
2020-12-22 11:51:54 +0800

    feat: 寄生组合继承

--- advancedJavaScript/example/gener.js
+++ advancedJavaScript/example/gener.js
@@ -1,6 +1,6 @@
-function Parent(name, actions) {
-  this.actions = actions
-  this.name = name
+function Parent (name, actions) {
+  this.name = name;
+  this.actions = actions;
 }
 
 Parent.prototype.eat = function () {
@@ -8,17 +8,21 @@ Parent.prototype.eat = function () {
 }
 
 function Child (id) {
-  Parent.apply(this, Array.from(arguments).slice(1))
+  Parent.apply(this, Array.from(arguments).slice(1));
   this.id = id;
 }
+// 模拟Object.create的效果
+// 如果直接使⽤用Object.create的话，
+// 可以写成Child.prototype = Object.create(Parent.prototype);
 
-Child.prototype = new Parent();
+let TemFunc = function () {};
+TemFunc.prototype = Parent.prototype;
+Child.prototype = new TemFunc();
 
 Child.prototype.constructor = Child;
 
 const child1 = new Child(1, "c1", ["hahahahahhah"]);
-
-const child2 = new Child(2, "c2", ["xixixixixixx"]);
+const child2 = new Child(2, "c2", ["xixixixixixx"])
 
 child1.eat(); // c1 - eat
 child2.eat(); // c2 - eat
