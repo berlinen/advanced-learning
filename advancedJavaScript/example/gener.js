3831f4bdd99665ec9dae74265d125e5dddb6d121
“berlinen” <1292393108@qq.com>
2020-12-22 11:29:23 +0800

    feat: 原型链继承；

--- advancedJavaScript/example/gener.js
+++ advancedJavaScript/example/gener.js
@@ -1,1 +1,16 @@
+function Parent () {
+  this.actions = ['eat', 'run']
+}
 
+function Child () {}
+
+Child.prototype = new Parent();
+Child.prototype.constructor = Child;
+
+const child1 = new Child();
+const child2 = new Child();
+
+child1.actions.pop();
+
+console.log(child1.actions)  // ['eat']
+console.log(child2.actions)  // ['eat']
