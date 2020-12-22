d345fa3feacb3c0295bf70368350d68902c410e8
“berlinen” <1292393108@qq.com>
2020-12-22 11:46:00 +0800

    feat: 组合继承

--- advancedJavaScript/example/gener.js
+++ advancedJavaScript/example/gener.js
@@ -1,16 +1,25 @@
 function Parent(name, actions) {
-  this.actions = actions;
-  this.name = name;
-  this.eat = function () {
-    console.log(`${name} - eat`);
-  }
+  this.actions = actions
+  this.name = name
 }
 
-function Child(id) {
-  Parent.apply(this, Array.prototype.slice.call(arguments, 1));
+Parent.prototype.eat = function () {
+  console.log(`${this.name} - eat`);
+}
+
+function Child (id) {
+  Parent.apply(this, Array.from(arguments).slice(1))
   this.id = id;
 }
 
-const child1 = new Child(1, "c1", ["eat"]);
-const child2 = new Child(2, "c2", ["sing", "jump", "rap"]);
-console.log(child1.eat === child2.eat); // false
+Child.prototype = new Parent();
+
+Child.prototype.constructor = Child;
+
+const child1 = new Child(1, "c1", ["hahahahahhah"]);
+
+const child2 = new Child(2, "c2", ["xixixixixixx"]);
+
+child1.eat(); // c1 - eat
+child2.eat(); // c2 - eat
+console.log(child1.eat === child2.eat); // true
