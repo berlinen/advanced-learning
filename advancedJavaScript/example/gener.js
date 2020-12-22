c1b6b03f1d34fc76b668f0d5b6e6e0111f306ac7
“berlinen” <1292393108@qq.com>
2020-12-22 11:38:46 +0800

    feat: 构造函数传参数

--- advancedJavaScript/example/gener.js
+++ advancedJavaScript/example/gener.js
@@ -1,16 +1,15 @@
-function Parent () {
-  this.actions = ['eat', 'run']
+function Parent(name, actions) {
+  this.actions = actions;
+  this.name = name;
 }
 
-function Child () {}
+function Child (id, name, actions) {
+  Parent.apply(this, Array.from(arguments).slice(1));
+  this.id = id;
+}
 
-Child.prototype = new Parent();
-Child.prototype.constructor = Child;
+const child1 = new Child(1, "c1", ["eat"]);
+const child2 = new Child(2, "c2", ["sing", "jump", "rap"]);
 
-const child1 = new Child();
-const child2 = new Child();
-
-child1.actions.pop();
-
-console.log(child1.actions)  // ['eat']
-console.log(child2.actions)  // ['eat']
+console.log(child1.name); // { actions: [ 'eat' ], name: 'c1', id: 1 }
+console.log(child2.name); // { actions: [ 'sing', 'jump', 'rap' ], name: 'c2', id: 2 }
