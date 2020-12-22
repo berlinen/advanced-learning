a23af9a639a2b7df6c75d35efc868a721b3c31cf
“berlinen” <1292393108@qq.com>
2020-12-22 11:41:06 +0800

    feat: 构造函数继承问题

--- advancedJavaScript/example/gener.js
+++ advancedJavaScript/example/gener.js
@@ -1,15 +1,16 @@
 function Parent(name, actions) {
   this.actions = actions;
   this.name = name;
+  this.eat = function () {
+    console.log(`${name} - eat`);
+  }
 }
 
-function Child (id, name, actions) {
-  Parent.apply(this, Array.from(arguments).slice(1));
+function Child(id) {
+  Parent.apply(this, Array.prototype.slice.call(arguments, 1));
   this.id = id;
 }
 
 const child1 = new Child(1, "c1", ["eat"]);
 const child2 = new Child(2, "c2", ["sing", "jump", "rap"]);
-
-console.log(child1.name); // { actions: [ 'eat' ], name: 'c1', id: 1 }
-console.log(child2.name); // { actions: [ 'sing', 'jump', 'rap' ], name: 'c2', id: 2 }
+console.log(child1.eat === child2.eat); // false
