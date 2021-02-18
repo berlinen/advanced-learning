39fed7a7c30d71e2ffd2210aa8dc535fac0209fb
“berlinen” <1292393108@qq.com>
2021-02-18 16:32:15 +0800

    feat: create action

--- advancedJavaScript/example/closure/index.js
+++ advancedJavaScript/example/closure/index.js
@@ -2,14 +2,20 @@
   function prepare() {
     const context = document.getElementById('content').getContext('2d');
     const heroImg = new Image();
+    let loaded = false;
     return {
       /**
        *
-       * @param {Function} [callback] 当准备好之后调用的回调函数
+       * @param {Function} [callback] - 当准备好之后调用的回调函数
        */
       getResource(callback) {
+        if(loaded) {
+          callback && callback(context, heroImg);
+          return;
+        }
         heroImg.onload = function () {
           callback && callback(context, heroImg);
+          loaded = true;
         };
         heroImg.src = './hero.png';
       }
@@ -17,7 +23,7 @@
   }
 
   // 画图
-  function drawHero(context, heroImg) {
+  function drawHero(context, heroImg, {initX, initY}) {
     var imgPos = {
       x: 0,
       y: 0,
@@ -26,8 +32,8 @@
     };
 
     var rect = {
-      x: 0,
-      y: 0,
+      x: initX,
+      y: initY,
       width: 40,
       height: 40
     };
@@ -48,7 +54,13 @@
 
   var resourceManager = prepare();
   resourceManager.getResource(function (context, heroImg) {
-    drawHero(context, heroImg);
+    drawHero(context, heroImg, {initX: 0, initY: 0});
+  });
+
+  document.getElementById('create').addEventListener('click', function () {
+    resourceManager.getResource(function (context, heroImg) {
+      drawHero(context, heroImg, {initX: Math.random() * 200, initY: Math.random() * 200});
+    });
   });
 
 })()
