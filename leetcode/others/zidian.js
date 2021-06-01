79067d8610a44af723c0c008bb223d22f4afdd97
berlinen <dice@terminus.io>
2021-06-01 11:34:32 +0800

    feat: 给定一个字符串，返回字典树种以该字符串开头的所有单词

--- leetcode/others/zidian.js
+++ leetcode/others/zidian.js
@@ -22,6 +22,35 @@ class PrefixTreeNode {
     }
     addWordHelper(this, str)
   }
+  // 给定一个字符串，返回字典树种以该字符串开头的所有单词
+  predictWord(str) {
+    let getRemainingTree = function(str, tree) {
+      let node = tree
+      while (str) {
+        node = node.children[str[0]]
+        str = str.substr(1)
+      }
+      return node
+    }
+    // 该数组维护所有以 str 开头的单词
+    let allWords = []
+    let allWordsHelper = function(stringSoFar, tree) {
+      for (let k in tree.children) {
+        const child = tree.children[k]
+        let newString = stringSoFar + child.value
+        if (child.endWord) {
+          allWords.push(newString)
+        }
+        allWordsHelper(newString, child)
+      }
+    }
+    let remainingTree = getRemainingTree(str, this)
+    if (remainingTree) {
+      allWordsHelper(str, remainingTree)
+    }
+    return allWords
+  }
+
 
 }
 
